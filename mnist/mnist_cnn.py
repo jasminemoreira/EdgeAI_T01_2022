bf4749ad5cebe161e0a5018036d5de16266cfc83
Jasmine Moreira <jasmine.moreira.2013@gmail.com>
2022-04-28 18:27:20 -0300

    mnist

---
+++ mnist/mnist_cnn.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 26 09:00:18 2018
+
+@author: jasmine
+
+1) Preparar dados
+2) Criar o modelo (input, output size, forward pass)
+3) Criar a função de erro (loss) e o otimizador 
+4) Criar o loop de treinamento
+   - forward pass: calcular a predição e o erro
+   - backward pass: calcular os gradientes
+   - update weights: ajuste dos pesos do modelo
+"""
+
+# Ajuste de memória para habilitar redes convolutivas
+#import tensorflow as tf
+#tf.config.experimental.set_memory_growth(tf.config.list_physical_devices('GPU')[0], True)
+
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras import models, layers
+from tensorflow.keras.utils import to_categorical
+
+# preparar dados
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+train_images.shape
+
+train_images = train_images.reshape((60000, 28, 28, 1))
+train_images = train_images.astype('float32')/255
+
+test_images = test_images.reshape((10000, 28, 28,1))
+test_images = test_images.astype('float32')/255
+
+train_labels = to_categorical(train_labels)
+test_labels = to_categorical(test_labels)
+
+
+# criar modelo
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3,3),activation='relu', input_shape=(28,28,1)))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64, (3,3),activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64, (3,3),activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation = 'relu'))
+model.add(layers.Dense(10, activation = 'softmax'))
+
+
+model.compile(optimizer='adam',loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.summary()
+
+history = model.fit(train_images, 
+                      train_labels, 
+                      epochs=10, 
+                      batch_size=128, 
+                      validation_data=(test_images,test_labels))
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print('test_acc: ', test_acc)
+
+
+import matplotlib.pyplot as plt
+history_dict = history.history
+loss_values = history_dict['loss']
+val_loss_values = history_dict['val_loss']
+
+epochs = range(1, len( history_dict['loss']) + 1)
+
+plt.plot(epochs, loss_values, 'bo', label='Training Loss')
+plt.plot(epochs, val_loss_values, 'b', label='Validation Loss')
+plt.title("Training and Validation Loss")
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.legend()
+plt.show()
+
+
+acc_values = history_dict['accuracy']
+val_acc_values = history_dict['val_accuracy']
+
+plt.plot(epochs, acc_values, 'bo', label='Training Acc')
+plt.plot(epochs, val_acc_values, 'b', label='Validation Acc')
+plt.title("Training and Validation Accuracy")
+plt.xlabel("Epochs")
+plt.ylabel("Accuracy")
+plt.legend()
+plt.show()
+
+
+digit = train_images[7]
+import matplotlib.pyplot as plt
+plt.imshow(digit, cmap=plt.cm.binary)
+
+
+
+
